6b430f1e7b1ee76e2c0f43abe83c96f72cb80d09
Ignacio Dolan <ignacio.dolan@gmail.com>
2020-04-26 23:24:07 +0200

    Shuffle cards and delivers to each player

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,13 @@ import socketIOClient from 'socket.io-client';
 const ENDPOINT = 'http://127.0.0.1:4001';
 
 const App = () => {
+  const [socket, setSocket] = React.useState(null);
   const [currentPlayers, setCurrentPlayers] = React.useState([]);
+  const [card, setCard] = React.useState(null);
 
   React.useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
+    setSocket(socket);
     socket.on('currentPlayers', (data) => {
       console.log(data);
       setCurrentPlayers(data);
@@ -14,19 +17,29 @@ const App = () => {
     socket.on('newPlayer', (data) => {
       console.log(data);
     });
+    socket.on('newCard', (data) => {
+      console.log(data);
+      setCard(data);
+    });
   }, []);
 
   React.useEffect(() => {}, [currentPlayers]);
 
+  const shuffle = () => {
+    socket.emit('shuffle');
+  };
+
   return (
-    <p>
-      Current players count: {currentPlayers.length}
+    <>
+      <p>Current players count: {currentPlayers.length}</p>
       <ul>
         {currentPlayers.map((player) => (
-          <li>{player.name}</li>
+          <li key={player.playerId}>{player.name}</li>
         ))}
       </ul>
-    </p>
+      <button onClick={shuffle}>Shuffle</button>
+      {card && <p>You got the {card} card</p>}
+    </>
   );
 };
 
--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const socketIo = require('socket.io');
 
 const port = process.env.PORT || 4001;
 const index = require('./routes/index');
+const shuffle = require('./helpers/shuffle');
 
 const app = express();
 app.use(index);
@@ -11,6 +12,18 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 const players = [];
+const cards = ['POLICE', 'DOCTOR', 'GOD', 'CITIZEN'];
+
+const suffleCards = () => {
+  console.log('shuffleing', cards);
+  const cardsToDeliver = shuffle(cards);
+  console.log('shuffled', cardsToDeliver);
+  for (let i = 0; i < players.length; i++) {
+    let player = players[i];
+    let card = cardsToDeliver.shift();
+    io.to(player.playerId).emit('newCard', card);
+  }
+};
 
 // socket.io server
 io.on('connection', function (socket) {
@@ -28,6 +41,7 @@ io.on('connection', function (socket) {
   // update all other players of the new player
   socket.broadcast.emit('newPlayer', players[socket.id]);
 
+  socket.on('shuffle', suffleCards);
   // when a player disconnects, remove them from our players object
   socket.on('disconnect', function () {
     console.log('user disconnected');
